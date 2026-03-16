fcf478c33b6efc6bf1e786fc6f51ccfbfc96b202
Marcus Vinícius Gonçalves Gois <marcus.gois@dcx.ufpb.br>
2026-03-16 11:25:41 -0300

    Update Banho.java

--- petshop/src/Banho.java
+++ petshop/src/Banho.java
@@ -1,16 +1,38 @@
 import java.util.LocalDate;
+import java.util.Objects;
 
-public class Banho extends ServicoBase{
+public class Banho implements {
 public enum TamPelo{
   Curto, Medio, Grande
 }
 
-private Tampelo pelo;
+    public Banho(double tampelo) {
+        Tampelo = tampelo;
+    }
 
-public Banho(LocalDate data, Tamanho tamanho, double preco, int cod, TamPelo pelo){super(data, tamanho, preco, codigo);
-   this.pelo = pelo;	
+    private double Tampelo;
 
+    @Override
+    public String toString() {
+        return "Banho{" +
+                "pelo=" + pelo +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Banho banho = (Banho) o;
+        return Objects.equals(Tampelo, banho.Tampelo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), Tampelo);
+    }
+
+    
 
 }
-}
+
