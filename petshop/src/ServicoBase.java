69694210ad1e8a2496f54dcbd1f8624cec8a35b0
Marcus Vinícius Gonçalves Gois <marcus.gois@dcx.ufpb.br>
2026-03-17 16:08:41 -0300

    Update ServicoBase.java

--- petshop/src/ServicoBase.java
+++ petshop/src/ServicoBase.java
@@ -1,50 +1,38 @@
 
 import java.time.LocalDate;
 import java.util.Objects;
+import java.time.LocalDate;
+import java.util.Objects;
 
-public abstract class ServicoBase implements ServicoCalculo {
- private LocalDate data = LocalDate.now();
- private Tamanho tamanho;
- private double preco;
- private int codigo;
+public abstract class ServicoBase implements ServicoIF {
+    protected int cod;
+    protected LocalDate data;
+    protected Tamanho tamanho;
+    protected double preco;
 
- public ServicoBase(int codigo,LocalDate data){
-   this.codigo;
-   this.data;	   
- }
-  public LocalDate getData(){
-  return data;
-  }
-  public Tamanho getTamanho(){
-  return tamanho;
-  }
-  public int codigo(){
-  return codigo;
-  }
-  @Override
-  public abstract double CalculoPreco(){
-   return preco;
-  }
-  @Override
-  public String getDescricao(){
-  return descricao;
-  }
-    public boolean equals(Object o) {
-        if (!(object instanceof ServicoBase)) return false;
-        if (!super.equals(object)) return false;
-        ServicoBase that = (ServicoBase) object;
-        return java.lang.Double.compare(preco, that.preco) == 0 && codigo == that.codigo && java.util.Objects.equals(data, that.data) && java.util.Objects.equals(tamanho, that.tamanho);
+    public ServicoBase(int cod, LocalDate data, Tamanho tamanho) {
+        this.cod = cod;
+        this.data = data;
+        this.tamanho = tamanho;
+        // O preço inicializa zerado e é definido pelo cálculo de cada subclasse
+        this.preco = calculaPreco(); 
     }
 
+    public int getCod() { return cod; }
+    public LocalDate getData() { return data; }
+    public Tamanho getTamanho() { return tamanho; }
+    public double getPreco() { return calculaPreco(); }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServicoBase that = (ServicoBase) o;
+        return cod == that.cod;
+    }
+
+    @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), data, tamanho, preco, codigo);
+        return Objects.hash(cod);
     }
 }
-
-
-
-
-
-
-
-	
